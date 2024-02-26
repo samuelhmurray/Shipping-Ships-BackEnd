9ba82c5cab5f5fead2bf22c9ba26a3185cd23225
Sam Murray <shm31094@gmail.com>
2024-02-26 16:57:43 -0500

    hello

--- json-server.py
+++ json-server.py
@@ -43,7 +43,9 @@ class JSONServer(HandleRequests):
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
         else:
-            return self.response("", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+            return self.response(
+                "", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
+            )
 
     def do_PUT(self):
         """Handle PUT requests from a client"""
@@ -53,7 +55,7 @@ class JSONServer(HandleRequests):
         pk = url["pk"]
 
         # Get the request body JSON for the new data
-        content_len = int(self.headers.get('content-length', 0))
+        content_len = int(self.headers.get("content-length", 0))
         request_body = self.rfile.read(content_len)
         request_body = json.loads(request_body)
 
@@ -61,21 +63,30 @@ class JSONServer(HandleRequests):
             if pk != 0:
                 successfully_updated = update_ship(pk, request_body)
                 if successfully_updated:
-                    return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
+                    return self.response(
+                        "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+                    )
 
         elif url["requested_resource"] == "docks":
             if pk != 0:
                 successfully_updated = update_dock(pk, request_body)
                 if successfully_updated:
-                    return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
+                    return self.response(
+                        "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+                    )
 
         if url["requested_resource"] == "haulers":
             if pk != 0:
                 successfully_updated = update_hauler(pk, request_body)
                 if successfully_updated:
-                    return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
+                    return self.response(
+                        "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+                    )
 
-        return self.response("Requested resource not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+        return self.response(
+            "Requested resource not found",
+            status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+        )
 
     def do_DELETE(self):
         """Handle DELETE requests from a client"""
@@ -87,47 +98,64 @@ class JSONServer(HandleRequests):
             if pk != 0:
                 successfully_deleted = delete_ship(pk)
                 if successfully_deleted:
-                    return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
+                    return self.response(
+                        "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+                    )
 
-                return self.response("Requested resource not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+                return self.response(
+                    "Requested resource not found",
+                    status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+                )
 
         elif url["requested_resource"] == "haulers":
             if pk != 0:
                 successfully_deleted = delete_hauler(pk)
                 if successfully_deleted:
-                    return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
+                    return self.response(
+                        "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+                    )
 
-                return self.response("Requested resource not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+                return self.response(
+                    "Requested resource not found",
+                    status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+                )
 
         elif url["requested_resource"] == "docks":
             if pk != 0:
                 successfully_deleted = delete_dock(pk)
                 if successfully_deleted:
-                    return self.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
+                    return self.response(
+                        "", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value
+                    )
 
-                return self.response("Requested resource not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+                return self.response(
+                    "Requested resource not found",
+                    status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value,
+                )
 
         else:
-            return self.response("Not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+            return self.response(
+                "Not found", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value
+            )
 
     def do_POST(self):
         """Handle POST requests from a client"""
+        url = self.parse_url(self.path)
+        pk = url["pk"]
+        
+        
+        if url["requested_resource"] == "ships":
 
-        pass
-
-
-
-
-
-
+        
 
 #
 # THE CODE BELOW THIS LINE IS NOT IMPORTANT FOR REACHING YOUR LEARNING OBJECTIVES
 #
 def main():
-    host = ''
+    host = ""
     port = 8000
     HTTPServer((host, port), JSONServer).serve_forever()
+
 
 if __name__ == "__main__":
     main()
--- views/dock_view.py
+++ views/dock_view.py
@@ -1,6 +1,23 @@
 import sqlite3
 import json
 
+
+def create_dock(id, dock_data):
+    with sqlite3.connect("./shipping.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+            UPDATE Dock
+                SET
+                    location = ?,
+                    capacity = ?
+            WHERE id = ?
+            """,
+            (dock_data["location"], dock_data["capacity"], id),
+        )
+
+
 def update_dock(id, dock_data):
     with sqlite3.connect("./shipping.db") as conn:
         db_cursor = conn.cursor()
@@ -13,10 +30,11 @@ def update_dock(id, dock_data):
                     capacity = ?
             WHERE id = ?
             """,
-            (dock_data['location'], dock_data['capacity'], id)
+            (dock_data["location"], dock_data["capacity"], id),
         )
 
     return True if db_cursor.rowcount > 0 else False
+
 
 def delete_dock(pk):
     with sqlite3.connect("./shipping.db") as conn:
@@ -24,9 +42,11 @@ def delete_dock(pk):
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
         DELETE FROM Dock WHERE id = ?
-        """, (pk,)
+        """,
+            (pk,),
         )
         number_of_rows_deleted = db_cursor.rowcount
 
@@ -40,17 +60,19 @@ def list_docks():
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
         SELECT
             d.id,
             d.location,
             d.capacity
         FROM Dock d
-        """)
+        """
+        )
         query_results = db_cursor.fetchall()
 
         # Initialize an empty list and then add each dictionary to it
-        docks=[]
+        docks = []
         for row in query_results:
             docks.append(dict(row))
 
@@ -59,6 +81,7 @@ def list_docks():
 
     return serialized_docks
 
+
 def retrieve_dock(pk):
     # Open a connection to the database
     with sqlite3.connect("./shipping.db") as conn:
@@ -66,14 +89,17 @@ def retrieve_dock(pk):
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
         SELECT
             d.id,
             d.location,
             d.capacity
         FROM Dock d
         WHERE d.id = ?
-        """, (pk,))
+        """,
+            (pk,),
+        )
         query_results = db_cursor.fetchone()
 
         # Serialize Python list to JSON encoded string
